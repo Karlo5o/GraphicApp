17a04fefbc2b27e10382192c8d8e58f32dcf6c21
Karel Vrabka <karel.vrabka@gmail.com>
2020-10-09 17:17:15 +0200

    First commit, project created.

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame as pg
+
+import matplotlib as mpl
+import numpy as np
+
+
+if __name__ == '__main__':
+    pg.init()
+    pg.display.set_mode((900, 1600))
+    pg.display.set_caption('Graphic tools GUI')
